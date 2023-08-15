867e6495bff1c8f33df235ef32a294f9304f7405
Aphrodis1st <aphrodisu2019@gmail.com>
2023-08-15 12:54:13 +0200

    My header file

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -9,13 +9,13 @@
  */
 
 
-struct dog
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
 
-};
+} dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
