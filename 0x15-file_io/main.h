f583f946eded834f70a02296c7320b00d30c404e
Aphrodis1st <aphrodis2019@gmail.com>
2023-09-04 20:09:22 +0200

    a program that displays the information contained in the ELF header at the start of an ELF file.

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -9,13 +9,41 @@
 #include <stdlib.h>
 #include <string.h>
 
+#include <elf.h>
 
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
 
+/** prototypes for pre last file*/
+
 char *create_buffer(char *file);
 void close_file(int fd);
+
+/** prototypes for the last file */
+
+void check_elf(unsigned char *e_ident);
+void print_magic(unsigned char *e_ident);
+void print_class(unsigned char *e_ident);
+void print_data(unsigned char *e_ident);
+void print_version(unsigned char *e_ident);
+void print_abi(unsigned char *e_ident);
+void print_osabi(unsigned char *e_ident);
+void print_type(unsigned int e_type, unsigned char *e_ident);
+void print_entry(unsigned long int e_entry, unsigned char *e_ident);
+void close_elf(int elf);
+
+
+check_elf(header->e_ident);
+printf("ELF Header:\n");
+print_magic(header->e_ident);
+print_class(header->e_ident);
+print_data(header->e_ident);
+print_version(header->e_ident);
+print_osabi(header->e_ident);
+print_abi(header->e_ident);
+print_type(header->e_type, header->e_ident);
+print_entry(header->e_entry, header->e_ident);
 
 
 #endif
